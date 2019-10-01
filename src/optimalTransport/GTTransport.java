72a7ebc969b9ce40aa1c2c706a12162f3e4a1933
mnj98 <mnj98@vt.edu>
2019-10-01 16:00:32 -0400

    update with greedyMatch test code

--- src/optimalTransport/GTTransport.java
+++ src/optimalTransport/GTTransport.java
@@ -5,8 +5,10 @@ package optimalTransport;
 
 
 public class GTTransport {
-	private static double[] vertexVisited;
+	private static int[] vertexVisited;
 	private static int AugPathVerIndex;
+	private static int[] bFree;
+	private static int[] aFree;
 	
 	public static double[][] Transport(double[][] C, double[] supplies, double[] demands, int n) {
 		
@@ -23,8 +25,8 @@ public class GTTransport {
 		double[][] CBA = transpose(C);
 		double[][] CAB = C;
 		
-		int[] bFree = new int[n];
-		int[] aFree = new int[n];
+		bFree = new int[n];
+		aFree = new int[n];
 		
 		for(int i = 0; i < n; i++) {
 			bFree[i] = 1;
@@ -58,7 +60,7 @@ public class GTTransport {
 		
 		double[][] slackAB = new double[n][n];
 		double[][] slackBA = new double[n][n];
-		vertexVisited = new double[2*n];
+		vertexVisited = new int[2*n];
 		double[] augmentingPathVertices = new double[2*n];
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; i < n; i++) {
@@ -82,11 +84,58 @@ public class GTTransport {
 		return t;
 	}
 	
-	public static int[] DFSUtil(int vertex, int[] augmentingPathVertices, int n, double[][] slackAB, double[][] slackBA, double[][] capacityAB, double[][] capaictyBA ){
+	//public int augPathVer
+	public int[] DFSUtil(int vertex, int[] augmentingPathVertices, int n, double[][] slackAB, double[][] slackBA, double[][] capacityAB, double[][] capacityBA ){
 		AugPathVerIndex = 0;
 		augmentingPathVertices[AugPathVerIndex] = vertex;
 		
-		//while(false) {}
+		while(AugPathVerIndex >= 0) {
+			vertex = augmentingPathVertices[AugPathVerIndex];
+			
+			
+			if(vertex > n && aFree[vertex - n] == 1) { // THERE MAY BE AN ISSUE WITH THIS CONDITION
+															// LOOK HERE WHEN IT DOESN'T WORK
+				return augmentingPathVertices;
+			}
+			
+			boolean backtrack = true;
+			
+			int range_var1 = vertexVisited[vertex]+1; //this +1 may be wrong too
+			int range_var2 = 0;
+			
+			if(vertex < n) {
+				range_var2 = 2*n;
+			}
+			else {
+				range_var2 = n;
+			}
+			
+			for(int i = range_var1; i <= range_var2; i++) {
+				vertexVisited[vertex] = i;
+				
+				
+				if(vertex < n) {
+					int a  = i - n;
+					if(slackBA[a][vertex] == 0 && capacityBA[a][vertex] > 0) {
+						backtrack = false;
+						augmentingPathVertices[++AugPathVerIndex] = i;
+						break;
+					}
+				}
+				else {
+					int a = vertex - n;
+					if(slackAB[i][a] == 0 && capacityAB[i][a] > 0) {
+						backtrack = false;
+						augmentingPathVertices[++AugPathVerIndex] = i;
+						break;
+					}
+				}
+			}
+			if(backtrack) {
+				augmentingPathVertices[AugPathVerIndex--] = 0;
+			}
+			
+		}
 		return augmentingPathVertices;
 	}
 	
